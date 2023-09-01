99984e08ddec048396478ecb333e06b72f1a15ab
Chris Beeley <chris.beeley@gmail.com>
2023-09-01 16:45:33 +0100

    Calculate dataframe outside of reactice

--- rtt-shiny.R
+++ rtt-shiny.R
@@ -25,6 +25,12 @@ latest_referrals_actual <- rtt_data[rtt_data$month_year == latest_data,]$new_ref
 latest_outflow_actual <- rtt_data[rtt_data$month_year == latest_data,]$total_activity
 waiting_list_at_pledge <- rtt_data[rtt_data$month_year == "2023-01-01",]$waiting_list
 
+# time dataframe
+
+time_df <- data.frame(month_year = prediction_time <- seq(latest_data, ymd("2025-01-01"), by = "months") #  get all the dates for the 20 months
+                      , month_no = seq(0, 19) # index for multiplying monthly rate -- start at 0 because not including june
+)
+
 # fixed assumptions
 jr_dr_perc_consultant_led <- 0.80
 jr_dr_daily_cancel <- 33100
@@ -70,19 +76,19 @@ ui <- fluidPage(
                                "Include seasonal variation in predictions?",
                                c("Yes - show seasonality" = "seasonal",
                                  "No - use straight lines" = "linear"
-                                ),
+                               ),
                                inline = FALSE,
                                selected = "linear"
-                    
+                               
                   ),
-        
+                  
                   # number to choose referrals increases
                   numericInput("referrals_change", 
                                "Referrals percent change per year", 
                                min = -100,
                                max = 100, 
                                value = 5
-                              ),
+                  ),
                   
                   # number to choose outflow increases
                   numericInput("outflow_change", 
@@ -90,7 +96,7 @@ ui <- fluidPage(
                                min = -100,
                                max = 100, 
                                value = 5
-                              ),
+                  ),
                   
                   # help text on outflow
                   helpText("The amount of change in outflow is calculated using the percentage change
@@ -103,7 +109,7 @@ ui <- fluidPage(
                                min = 0,
                                max = 57, 
                                value = 6 
-                             ),
+                  ),
                   
                   # number of consultant strike days to include
                   numericInput("consultant", 
@@ -111,15 +117,15 @@ ui <- fluidPage(
                                min = 0,
                                max = 38, 
                                value = 4 
-                              ),
-                 
-                   # strike intensity
+                  ),
+                  
+                  # strike intensity
                   numericInput("intensity", 
                                "Strike intensity %", 
                                min = 0,
                                max = 100, 
                                value = 85
-                              ),
+                  ),
                   
                   # help text on strikes
                   helpText("Strike days will be incorporated in the first few months, 
@@ -127,7 +133,7 @@ ui <- fluidPage(
                            (for consultants) until the number of inputted strike days is reached"),
                   
                   width = 2
-              
+                  
                 ),
                 
                 # add graph and title in main panel
@@ -140,7 +146,7 @@ ui <- fluidPage(
                   plotOutput("waiting_list_plot")
                   
                 )
-    
+                
   )
 )
 
@@ -152,63 +158,61 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   #### Make predictions ####
-
+  
   # Make a reactive dataframe of months to calculate for (up to Jan 25)
   predictions <- reactive(
     {
-    data.frame(month_year = prediction_time <- seq(latest_data, ymd("2025-01-01"), by = "months") #  get all the dates for the 20 months
-      , month_no = seq(0, 19) # index for multiplying monthly rate -- start at 0 because not including june
-      )  %>%
-      
-      # calculate predictions for referrals, outflow, and waiting list (not including seasonality or strikes)
-      # here we get the predicted values using the final value of the linear trend (rather than actual value) 
-      mutate(referrals_pred = latest_referrals * monthlyRate(input$referrals_change)^month_no # get referrals increases
+      time_df %>%
+        
+        # calculate predictions for referrals, outflow, and waiting list (not including seasonality or strikes)
+        # here we get the predicted values using the final value of the linear trend (rather than actual value) 
+        mutate(referrals_pred = latest_referrals * monthlyRate(input$referrals_change)^month_no # get referrals increases
                , outflow_pred = latest_outflow * monthlyRate(input$outflow_change)^month_no # here we assume all outflow increases by this (i.e. missing data important)
-              ) %>% 
+        ) %>% 
         
-      # include effect of seasonality
-      mutate(month = month(month_year)) %>% 
-      left_join(seasonality, by = "month") %>% 
-      mutate(referrals_pred_seasonal = if_else(month_no == 0, latest_referrals_actual, referrals_pred * referrals_seasonality)
-             , outflow_pred_seasonal = if_else(month_no == 0, latest_outflow_actual, outflow_pred * activity_seasonality)) %>% 
+        # include effect of seasonality
+        mutate(month = month(month_year)) %>% 
+        left_join(seasonality, by = "month") %>% 
+        mutate(referrals_pred_seasonal = if_else(month_no == 0, latest_referrals_actual, referrals_pred * referrals_seasonality)
+               , outflow_pred_seasonal = if_else(month_no == 0, latest_outflow_actual, outflow_pred * activity_seasonality)) %>% 
         
-      # include effect of strikes
+        # include effect of strikes
         # if month index is less than the round-up input value of strike days (divided by ), don't add strike days
         # if it is equal to number of round-up input, assign the remainder of days. otherwise give a 3.
-      mutate(jr_dr_strike_days = case_when(ceiling(input$jr_drs/3) > month_no & month_no > 0 ~ 3
-                                           , ceiling(input$jr_drs/3) == month_no ~ input$jr_drs %% 3
-                                           , TRUE ~ 0)
-             , consultant_strike_days =  case_when(ceiling(input$consultant/2) > month_no & month_no > 0 ~ 2
-                                                   , ceiling(input$consultant/2) == month_no ~ input$consultant %% 2
-                                                   , TRUE ~ 0)
-             , jr_dr_cancellations = jr_dr_strike_days * jr_dr_perc_consultant_led * jr_dr_daily_cancel * (input$intensity/100)^(month_no)
-             , consultant_cancellations = consultant_strike_days * consultant_daily_cancel * (input$intensity/100)^(month_no)
-             ) %>%
-
-      mutate(outflow_pred = outflow_pred - jr_dr_cancellations - consultant_cancellations
-             , outflow_pred_seasonal = outflow_pred_seasonal - jr_dr_cancellations - consultant_cancellations) %>%
-      
-      # get new waiting list number
-      # cumulative sum of referrals (up to t-1), - outflow (at t-1), and adding to june 23 waiting list
+        mutate(jr_dr_strike_days = case_when(ceiling(input$jr_drs/3) > month_no & month_no > 0 ~ 3
+                                             , ceiling(input$jr_drs/3) == month_no ~ input$jr_drs %% 3
+                                             , TRUE ~ 0)
+               , consultant_strike_days =  case_when(ceiling(input$consultant/2) > month_no & month_no > 0 ~ 2
+                                                     , ceiling(input$consultant/2) == month_no ~ input$consultant %% 2
+                                                     , TRUE ~ 0)
+               , jr_dr_cancellations = jr_dr_strike_days * jr_dr_perc_consultant_led * jr_dr_daily_cancel * (input$intensity/100)^(month_no)
+               , consultant_cancellations = consultant_strike_days * consultant_daily_cancel * (input$intensity/100)^(month_no)
+        ) %>%
         
-      mutate(waiting_list_pred = latest_waitlist + cumsum(lag(referrals_pred, default = 0)) - cumsum(lag(outflow_pred, default = 0))
-             , waiting_list_pred_seasonal = latest_waitlist + cumsum(lag(referrals_pred_seasonal, default = 0)) - cumsum(lag(outflow_pred_seasonal, default = 0))) %>% 
+        mutate(outflow_pred = outflow_pred - jr_dr_cancellations - consultant_cancellations
+               , outflow_pred_seasonal = outflow_pred_seasonal - jr_dr_cancellations - consultant_cancellations) %>%
         
-      # join the original dataset
-      full_join(rtt_data, by = "month_year") 
+        # get new waiting list number
+        # cumulative sum of referrals (up to t-1), - outflow (at t-1), and adding to june 23 waiting list
+        
+        mutate(waiting_list_pred = latest_waitlist + cumsum(lag(referrals_pred, default = 0)) - cumsum(lag(outflow_pred, default = 0))
+               , waiting_list_pred_seasonal = latest_waitlist + cumsum(lag(referrals_pred_seasonal, default = 0)) - cumsum(lag(outflow_pred_seasonal, default = 0))) %>% 
+        
+        # join the original dataset
+        full_join(rtt_data, by = "month_year") 
     }
     
   )
   
-
-    
+  
+  
   #### Referrals and completed plot ####
   # To do: can the if/else be implemented into one ggplot code so easier to update with colours etc.?
   
   output$referrals_plot <- renderPlot(
     { 
       if (input$seasonality == "linear") {
-      # Plot referrals and completeds on same graph
+        # Plot referrals and completeds on same graph
         predictions() %>% 
           ggplot(aes(x = month_year)) +
           geom_line(aes(y = total_activity, color = "Total outflow"), size = linesize) +
@@ -259,16 +263,16 @@ server <- function(input, output) {
           annotate("rect", xmin = ymd("2024-12-01"), xmax = ymd("2025-01-01"), ymin = 0, ymax = Inf, fill = "grey", alpha = 0.2) +
           annotate("text", x = ymd("2020-02-15"), y = 250000, label = "COVID-19", angle = 90) +
           annotate("text", x = ymd("2024-11-15"), y = 700000, label = "Deadline for next general election", angle = 90)
-          
-      }
         
+      }
+      
     }
   )
   
   #### Waiting list plot ####
   
   # Predict waiting list
-
+  
   output$waiting_list_plot <- renderPlot(
     {
       if (input$seasonality == "linear"){
@@ -294,7 +298,7 @@ server <- function(input, output) {
           geom_segment(aes(x = ymd("2023-01-01"), xend = ymd("2025-01-01"), y = waiting_list_at_pledge, yend = waiting_list_at_pledge), linetype = 2, color = "white", alpha = 0.8)
         
       }
-
+      
       else {
         # Plot waiting list with seasonality
         predictions() %>%
